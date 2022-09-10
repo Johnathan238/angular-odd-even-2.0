562cc175baf429a8a91948c3303308db05cb0200
Johnathan238 <60320817+Johnathan238@users.noreply.github.com>
2022-09-10 17:39:17 -0400

    Saving a dropDown directive

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -16,3 +16,18 @@ export class unlessDirective{
   }
   
 }
+
+
+////////////////////DropDown Directive///////////////////////////////////
+// import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+ 
+// @Directive({
+//   selector: '[appDropdown]'
+// })
+// export class DropdownDirective {
+//   @HostBinding('class.open') isOpen = false;
+//   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+//     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+//   }
+//   constructor(private elRef: ElementRef) {}
+// }
